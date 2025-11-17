27b54da01733abe76d3246c4e0a34f55904ce43e
swapnil2410 <swapnilnigam2410@gmail.com>
2025-11-17 20:07:19 +0530

    changes 2

--- src/main/java/com/example/Jenkin/Project/Controller.java
+++ src/main/java/com/example/Jenkin/Project/Controller.java
@@ -1,4 +1,15 @@
 package com.example.Jenkin.Project;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+
+    @GetMapping()
+    public void getStatus(){
+        System.out.println("Get Mapping changes 2");
+    }
 }
