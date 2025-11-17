c3e2b18065da99a6bab50c005271be661bc093f0
swapnil2410 <swapnilnigam2410@gmail.com>
2025-11-17 18:21:30 +0530

    Initial Spring Boot project upload

---
+++ src/test/java/com/example/Jenkin/Project/JenkinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jenkin.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
